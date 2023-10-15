e3fd31a10192c4348698e92c00e3e4c9f0d31b8f
Addison <addisonpickrell@gmail.com>
2023-10-15 07:58:15 -0700

    Update main.py
    Added Frequency Tables Function and Additional Dependencies

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 #Addison Reminder: CITATION NEEDED FOR ALL MODULES + TRAINING DATA SETS
 import spacy
 import pandas as pd
+import gensim
+import re
+from collections import Counter
 
 #Load Core Libaries
 nlpen = spacy.load("en_core_web_trf")
@@ -12,9 +15,20 @@ eng = spacy.lang.en.English()
 esp = spacy.lang.es.Spanish()
 
 #Preprocessing Function: Turns Pandas DataFrame Object into a String for NLP
-def dataframetostring(df):
+def d2s(df):
     return df.to_string(columns=None, buf=None, index=False, na_rep="NaN")
 
-# Commented Currently for Functionality
-# pand = pd.read_csv("[INSERT DATA LINK HERE]", usecols=[INSERT COLUMNS HERE])
-# string = dataframetostring(pand)
+def frequency(txt):
+    nouns = [token.text for token in txt if (not token.is_stop and not token.is_punct and token.pos_ == "NOUN")]
+    adjectives = [token.text for token in txt if (not token.is_stop and not token.is_punct and token.pos_ == "ADJ")]
+    verbs = [token.text for token in txt if (not token.is_stop and not token.is_punct and token.pos_ == "VERB")]
+    
+    #The Bottom Print Section is for Beta Testing; Will be Replaced with an Export-To-CSV Function
+    print("Most Common Nouns")
+    print(Counter(nouns).most_common(20))
+    print("Most Common Adjectives")
+    print(Counter(adjectives).most_common(20))
+    print("Most Common Verbs")
+    print(Counter(verbs).most_common(20))
+
+rawdata = nlpes(re.sub(" +", " ", d2s(pd.read_csv("INSERT DATA HERE", usecols=['INSERT COLS HERE'])).replace("\n"," ").replace("NaN", "")))
